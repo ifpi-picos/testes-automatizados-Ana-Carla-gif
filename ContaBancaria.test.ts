026dde14bb28f19b791ff50c9eb8da2e0cbf6c47
Ana_Carla_Guimarães <annacarlabarbosaguimaraes49@gmail.com>
2024-09-17 10:41:41 -0300

    Atividades Testes

--- ContaBancaria.test.ts
+++ ContaBancaria.test.ts
@@ -1,4 +1,96 @@
-// import { beforeEach, describe, expect, test } from "bun:test";
-// import ContaBancaria from "./ContaBancaria.ts";
+import { beforeEach, describe, expect, test } from "bun:test";
+import ContaBancaria from "./ContaBancaria.ts";
+
+// Antes de cad ateste vamos criar as contas  utilizando o beforEach
+
+describe ("TESTE DA CLASSE CONTA BANCÁRIA " ,() => {
+    let conta : ContaBancaria
+    beforeEach(() => { 
+     conta= new ContaBancaria ()
+    }) // UTILIZA ISSO PARA CRIAR UMA CONTA , PARA NÃO TER QUE FICAR REPETINDO DURANTE O PROGRAMA
+test("Método Depositar", () =>{
+    expect(conta.depositar(300)).toBe(300)
+})
+test ("Método Depositar - depósito inválido",()=> {
+    expect (()  => conta.depositar (-100)).toThrow("Valor inválido!"); // toThrow vai pegar o erro qeu ja foi lançado
+})
+
+ // coloca o parentese com espaço so qaundo coloca toThrow
+test ("Método Sacar- depositar", () => {
+    //expect(() => conta.sacar
+    conta.depositar(50)
+    expect(conta.sacar(30)). toBe(20);
+})
+
+test ("Método Sacar ", () => {
+    //expect(() => conta.sacar
+    
+    expect(() => conta.sacar (-1)).toThrow("Valor inválido");
+
+})
+
+}) 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// describe("Testando classe ContaBancaria", () => {
+//   let conta: ContaBancaria;
+//   beforeEach(() => {
+//     conta = new ContaBancaria();
+//   });
+//   test("Testando sacar um valor insuficiente " , () => {
+
+
+//     conta.sacar(500)
+//     expect (conta.consultarSaldo()).toBe
+//   })
+
+//   test("Testando depositar valor positivo", () => {
+//     conta.depositar(100);
+//     expect(conta.consultarSaldo()).toBe(100);
+//   });
+
+//   test("Testando depositar valor negativo", () => {
+//     conta.depositar(-100);
+//     expect(conta.consultarSaldo()).toBe(0);
+//   });
+// });
--- ContaBancaria.ts
+++ ContaBancaria.ts
@@ -1,1 +1,77 @@
-export default class ContaBancaria {}
+export default class ContaBancaria {
+private numeroConta: number  //Representa o número da conta bancária.
+private agencia:  number    // Representa o número da agência bancária.
+private saldo: number      // Armazena o saldo atual da conta.
+private extrato: string[] //Armazena o histórico de operações realizadas na conta (depósitos, saques, transferências).
+
+// UTILIZA O THIS PARA ACESSAR O QUE ESTÁ DENTRO DOS PRIVADOS
+
+  public constructor(){ // UTILIZA-SE ISSO PARA NÃO MOSTRAR OS TRACINHOS VERMELHOS (ERRADO)
+    this.numeroConta = 0
+    this.agencia = 0
+    this.saldo = 0
+    this.extrato =[]
+  } 
+  public depositar (valor : number){ // publico para aparecer para o usuário
+    if (valor > 0) {
+      this.saldo += valor ;
+      this.extrato.push (`Depósito de R$ ${valor.toFixed(2)}`) // Pega o extrato e utiliza o push para a acrescentar o valor novo  ao extrato.
+
+      // Utiliza-se toFixed para imprimir os dois númertos após a virgúla.
+      return this.saldo
+    }else {
+      throw new Error ("Valor inválido!"); // Criando um erro para lançar esse erro no else , colocamos por conta do teste
+    }
+  }  
+
+  public sacar (valor : number){
+    if (valor > 0  && valor <= this.saldo){
+      this.saldo -= valor 
+      return this.saldo
+    } else {
+        throw new Error ("Valor inválido!")
+    }
+  }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   public depositar(valor: number) {
+//     if (valor > 0) {
+//       this.saldo += valor;
+//     }
+//   }
+
+//   public sacar (valor:number){
+//     if( valor >  this.saldo ){
+//       return "Saldo insuficiente !"
+//     }
+//     else if (this.saldo - valor) {
+//       return "Saque efetuado com sucesso"
+//     }
+//   }
+
+//   public consultarSaldo() {
+//     return this.saldo;
+//   }
+// }
